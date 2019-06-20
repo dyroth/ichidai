cfa9a35557f3b3faaed38042ccfe85d83f746d06
Kristof Van Aken <kristof.van.aken@takethelead.be>
2019-06-21 00:11:00 +0200

    Baseline

---
+++ resources/views/_partials/master.blade.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ichi Dai - Kyokushin</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="/img/favicon.png">
+
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500">
+    {{--    <link rel="stylesheet" href="fonts/icomoon/style.css">--}}
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+    <div class="site-navbar-wrap bg-white">
+        <div class="site-navbar-top">
+            <div class="container py-2">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <a href="#" class="p-2 pl-0"><span class="icon-twitter"></span></a>
+                        <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2 pl-0"><span class="icon-linkedin"></span></a>
+                        <a href="#" class="p-2 pl-0"><span class="icon-instagram"></span></a>
+                    </div>
+                    <div class="col-6">
+                        <div class="d-flex ml-auto">
+                            <a href="#" class="d-flex align-items-center ml-auto mr-4">
+                                <span class="icon-envelope mr-2"></span>
+                                <span class="d-none d-md-inline-block">info@ichidai.be</span>
+                            </a>
+                            <a href="tel://12912830302" class="d-flex align-items-center">
+                                <span class="icon-phone mr-2"></span>
+                                <span class="d-none d-md-inline-block">+32 496 10 60 90</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="site-navbar-wrap bg-white">
+
+        <div class="container">
+            <div class="site-navbar bg-light">
+                <div class="py-1">
+                    <div class="row align-items-center">
+                        <div class="col-2">
+                            <h2 class="mb-0 site-logo"><a href="#">Ichi Dai</a></h2>
+                        </div>
+                        <div class="col-10">
+                            <nav class="site-navigation text-right" role="navigation">
+                                <div class="container">
+                                    <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
+                                                                                                  class="site-menu-toggle js-menu-toggle text-black"><span
+                                                    class="icon-menu h3"></span></a></div>
+
+                                    <ul class="site-menu js-clone-nav d-none d-lg-block">
+                                        <li class="active">
+                                            <a href="index.html">Ichi Dai</a>
+                                        </li>
+                                        <li class="has-children">
+                                            <a href="classes.html">Lessen</a>
+                                            <ul class="dropdown">
+                                                <li><a href="classes.html">Kumite</a></li>
+                                                <li><a href="classes.html">Kihon & Kata</a></li>
+                                                <li><a href="classes.html">Lesuren & Tarieven</a></li>
+                                                {{--<li class="has-children">--}}
+                                                {{--    <a href="classes.html">Weight Lifting</a>--}}
+                                                {{--    <ul class="dropdown">--}}
+                                                {{--        <li><a href="classes.html">Men</a></li>--}}
+                                                {{--        <li><a href="classes.html">Women</a></li>--}}
+                                                {{--        <li><a href="classes.html">Children</a></li>--}}
+                                                {{--    </ul>--}}
+                                                {{--</li>--}}
+
+                                            </ul>
+                                        </li>
+                                        <li><a href="news.html">Nieuws</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @yield('content')
+
+    <footer class="site-footer">
+        <div class="container">
+
+
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="row">
+                        <div class="col-6 col-md-4 col-lg-8 mb-5 mb-lg-0">
+                            <h3 class="footer-heading mb-4 text-primary">About</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos rem ullam,
+                                placeat amet sint vel impedit reprehenderit eius expedita nemo consequatur obcaecati
+                                aperiam, blanditiis quia iste in! Assumenda, odio?</p>
+                            <p><a href="#" class="btn btn-primary pill text-white px-4">Read More</a></p>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4 mb-5 mb-lg-0">
+                            <h3 class="footer-heading mb-4 text-primary">Quick Menu</h3>
+                            <ul class="list-unstyled">
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Approach</a></li>
+                                <li><a href="#">Sustainability</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Careers</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="row mb-5">
+                        <div class="col-md-12"><h3 class="footer-heading mb-4 text-primary">Contact Info</h3></div>
+                        <div class="col-md-6">
+                            <p>New York - 2398 <br> 10 Hadson Carl Street</p>
+                        </div>
+                        <div class="col-md-6">
+                            Tel. +32 496 10 60 90 <br>
+                            Mail. info@ichidai.be
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12"><h3 class="footer-heading mb-4 text-primary">Social Icons</h3></div>
+                        <div class="col-md-12">
+                            <p>
+                                <a href="#" class="pb-2 pr-2 pl-0"><span class="icon-facebook"></span></a>
+                                <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                                <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+                                <a href="#" class="p-2"><span class="icon-vimeo"></span></a>
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row pt-5 mt-5 text-center">
+                <div class="col-md-12">
+                    <p>
+                        Copyright &copy;
+                        <script>document.write(new Date().getFullYear());</script>
+                        All Rights Reserved.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</div>
+
+<script src="/js/jquery-3.3.1.min.js"></script>
+<script src="/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/owl.carousel.min.js"></script>
+<script src="/js/jquery.stellar.min.js"></script>
+<script src="/js/jquery.countdown.min.js"></script>
+<script src="/js/jquery.magnific-popup.min.js"></script>
+<script src="/js/bootstrap-datepicker.min.js"></script>
+<script src="/js/aos.js"></script>
+
+<script src="/js/main.js"></script>
+
+</body>
+</html>
