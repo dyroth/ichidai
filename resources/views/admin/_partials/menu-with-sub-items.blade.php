9cb09b347bb41c26242b3c481110af3b28bd30b7
Kristof Van Aken <vanakenk@gmail.com>
2020-08-16 15:21:48 +0200

    CMS overhaul - Better performance & use of template engine

---
+++ resources/views/admin/_partials/menu-with-sub-items.blade.php
@@ -0,0 +1,12 @@
+<ul class="nav metismenu" id="side-menu" style="padding-left:0px;">
+    <li class="{{ in_array($title, array_keys($config['items'])) ? 'active' : '' }}">
+        <a href="#"><i class="fa fa-files-o"></i> <span class="nav-label">{{ $config['title'] }}</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+            @foreach($config['items'] as $itemTitle => $item)
+                <li {{ $title == $itemTitle ? 'class=active' : '' }}>
+                    <a href="{{ route($item['route']) }}"><i class="{{ $item['icon'] }}"></i> <span class="nav-label">{{ $itemTitle }}</span></a>
+                </li>
+            @endforeach
+        </ul>
+    </li>
+</ul>
