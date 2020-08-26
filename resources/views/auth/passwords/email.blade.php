d5738e3f1a31c3c1a4cce450573183ae344fb6ee
Kristof Van Aken <vanakenk@gmail.com>
2020-08-27 00:12:57 +0200

    Added translation

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,13 +5,13 @@
   <div class="row">
     <div class="col-md-12">
       <div class="ibox-content">
-        <h2 class="font-bold">Forgot password</h2>
+        <h2 class="font-bold">Wachtwoord vergeten</h2>
         <div class="row">
           <div class="col-lg-12">
             <form class="m-t" role="form" method="POST" action="{{ route('password.email') }}">
               {{ csrf_field() }}
               <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email address" required>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email adres" required>
                 @if ($errors->has('email'))
                     <span class="help-block">
                         <strong>{{ $errors->first('email') }}</strong>
@@ -19,7 +19,7 @@
                 @endif
               </div>
               <div class="form-group">
-                <button type="submit" class="btn btn-primary block full-width m-b">Send new password</button>
+                <button type="submit" class="btn btn-primary block full-width m-b">Stuur nieuw wachtwoord</button>
               </div>
             </form>
           </div>
